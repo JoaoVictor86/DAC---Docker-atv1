ae06b9f4bbd1ef47b41f2255864c3bc5b06562bc
JoaoVictor86 <70730125+JoaoVictor86@users.noreply.github.com>
2022-10-10 19:47:18 -0300

    atualização final

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Atividade de Docker com Python.'
+
+@app.route('/pessoas/<string:nome>/<string:cidade>')
+def pessoa(nome,cidade):
+    return jsonify({'nome':nome,'cidade':cidade})
